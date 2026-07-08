d55506190374906744466af59e6b479fa55caa7c
winwpr0 <uksusov378@gmail.com>
2026-07-08 12:33:44 +0300

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,29 +1,32 @@
-/* kernel.c - Первое ядро xStep OS */
-/* Версия: 26.1a / a1.0_build1 */
+/* kernel.c - Первое ядро xStep OS
+ * Версия: 26.1a / a1.0_build1
+ */
 
 /* 1. Мультизагрузочный заголовок для GRUB */
-asm (
+__asm__(
     ".section .multiboot\n"
     ".align 4\n"
-    ".long 0x1BADB002\n"   /* Магическое число */
-    ".long 0x03\n"         /* Флаги */
+    ".long 0x1BADB002\n"           /* Магическое число */
+    ".long 0x03\n"                 /* Флаги */
     ".long -(0x1BADB002 + 0x03)\n" /* Контрольная сумма */
     ".section .text\n"
 );
 
 /* 2. Точка входа в ядро */
-void kernel_main() {
+void kernel_main(void) {
     /* 3. Выводим приветствие в видеопамять (0xB8000) */
-    char* video = (char*) 0xB8000;
-    const char* msg = "Welcome to xStep OS! Version 26.1a";
+    volatile char* video = (volatile char*)0xB8000;
+    const char* msg = "                                                                                                                                                                                                                                                                                                                                                                                                                            Starting xStep...                                                                                                                                                                                                        ";
     int i = 0;
 
     while (msg[i] != '\0') {
-        video[i * 2] = msg[i];     /* Символ */
-        video[i * 2 + 1] = 0x0F;   /* Белый текст на чёрном фоне */
+        video[i * 2]     = msg[i];   /* Символ */
+        video[i * 2 + 1] = 0x0F;     /* Белый текст на чёрном фоне */
         i++;
     }
 
     /* 4. Останавливаемся (пока нет обработки прерываний) */
-    while(1);
+    while (1) {
+        __asm__ volatile ("hlt");
+    }
 }

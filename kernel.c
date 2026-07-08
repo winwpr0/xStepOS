8ba8695345c9245ddc0d96c3acd692ecb371b2fb
winwpr0 <uksusov378@gmail.com>
2026-07-08 08:31:59 +0300

    Create Kernel

---
+++ kernel.c
@@ -0,0 +1,29 @@
+/* kernel.c - Первое ядро xStep OS */
+/* Версия: 26.1a / a1.0_build1 */
+
+/* 1. Мультизагрузочный заголовок для GRUB */
+asm (
+    ".section .multiboot\n"
+    ".align 4\n"
+    ".long 0x1BADB002\n"   /* Магическое число */
+    ".long 0x03\n"         /* Флаги */
+    ".long -(0x1BADB002 + 0x03)\n" /* Контрольная сумма */
+    ".section .text\n"
+);
+
+/* 2. Точка входа в ядро */
+void kernel_main() {
+    /* 3. Выводим приветствие в видеопамять (0xB8000) */
+    char* video = (char*) 0xB8000;
+    const char* msg = "Welcome to xStep OS! Version 26.1a";
+    int i = 0;
+
+    while (msg[i] != '\0') {
+        video[i * 2] = msg[i];     /* Символ */
+        video[i * 2 + 1] = 0x0F;   /* Белый текст на чёрном фоне */
+        i++;
+    }
+
+    /* 4. Останавливаемся (пока нет обработки прерываний) */
+    while(1);
+}
